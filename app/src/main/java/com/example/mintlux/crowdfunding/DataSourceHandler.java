48cde496e9122f5feae8df81437bae102fafb0f4
miriamelia <miriam@elianet.de>
2017-06-12 19:06:17 +0200

    activity_editprofile.xml activity_login activity_transfer DBs added

--- app/src/main/java/com/example/mintlux/crowdfunding/DataSourceHandler.java
+++ app/src/main/java/com/example/mintlux/crowdfunding/DataSourceHandler.java
@@ -19,16 +19,16 @@ public class DataSourceHandler {
     private SQLiteDatabase db;
     private DatabaseHelper dbHelp;
 
-    private String[] columns = {
-      DatabaseHelper.COL_1,
-      DatabaseHelper.COL_2,
-      DatabaseHelper.COL_3
-    };
-
     public DataSourceHandler(Context context) {
         Log.d(LOG_TAG, "DBSourceHandler creates DBHelper");
         dbHelp = new DatabaseHelper(context);
     }
+
+    private String[] columns = {
+            DatabaseHelper.USER_COL1,
+            DatabaseHelper.USER_COL2,
+            DatabaseHelper.USER_COL3
+    };
 
     public void open() {
         Log.d(LOG_TAG, "Referenz auf Datenbank ? ");
@@ -72,21 +72,20 @@ public class DataSourceHandler {
         for(int i = 0; i < list1.size(); i++) {
             User u = list1.get(i);
             String checkName = u.getUserName();
-            String checkPw = u.getPassword();
-            if(checkName.equals(userName) && checkPw.equals(password)) {
+            if(checkName.equals(userName)) {
                 return u;
             }
         }
 
             ContentValues values = new ContentValues();
-            values.put(DatabaseHelper.COL_2, userName);
-            values.put(DatabaseHelper.COL_3, password);
+            values.put(DatabaseHelper.USER_COL2, userName);
+            values.put(DatabaseHelper.USER_COL3, password);
 
 
             long insertId = db.insert(DatabaseHelper.TABLE_USER, null, values);
 
             Cursor cursor = db.query(DatabaseHelper.TABLE_USER,
-                    columns, DatabaseHelper.COL_1 + "=" + insertId,
+                    columns, DatabaseHelper.USER_COL1 + "=" + insertId,
                     null, null, null, null);
 
             cursor.moveToFirst();
@@ -98,25 +97,24 @@ public class DataSourceHandler {
         return user;
     }
 
-    public void removeFromDB(String username, String password) {
+    public void removeFromDB(String username) {
         List<User> list = this.getAllUsers();
         for (int i = 0; i < list.size(); i++) {
             User u = list.get(i);
             String checkName = u.getUserName();
-            String checkPw = u.getPassword();
-            if (checkName.equals(username) && checkPw.equals(password)) {
+            if (checkName.equals(username)) {
                 int help = u.getUserID();
                 db.execSQL("DELETE FROM User WHERE userID=" + help);
-                //dbHelp.onUpgrade(db, 1, 2);
+                dbHelp.onUpgrade(db, 1, 2);
             }
         }
     }
 
 
     private User cursorToUser(Cursor cursor) {                                      //wandelt Objekt aus DB in Objekt Klasse user um
-        int idIndex = cursor.getColumnIndex(DatabaseHelper.COL_1);
-        int idUserName = cursor.getColumnIndex(DatabaseHelper.COL_2);
-        int idPassword = cursor.getColumnIndex(DatabaseHelper.COL_3);
+        int idIndex = cursor.getColumnIndex(DatabaseHelper.USER_COL1);
+        int idUserName = cursor.getColumnIndex(DatabaseHelper.USER_COL2);
+        int idPassword = cursor.getColumnIndex(DatabaseHelper.USER_COL3);
 
         String name = cursor.getString(idUserName);
         String password = cursor.getString(idPassword);
